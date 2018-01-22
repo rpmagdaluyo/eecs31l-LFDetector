4332f973591f8dd380579231ecf38203b720bd14
Richard <rpmagdaluyo@gmail.com>
2018-01-22 15:07:44 -0800

    PC Work
    Finished the Lab write-up.

--- Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/xsim_webtalk.tcl
+++ Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/xsim_webtalk.tcl
@@ -1,6 +1,6 @@
-webtalk_init -webtalk_dir C:/Users/Richard/Documents/Schoolwork/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/
+webtalk_init -webtalk_dir C:/Users/Richard/Documents/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/
 webtalk_register_client -client project
-webtalk_add_data -client project -key date_generated -value "Sun Jan 21 21:34:39 2018" -context "software_version_and_target_device"
+webtalk_add_data -client project -key date_generated -value "Mon Jan 22 13:21:43 2018" -context "software_version_and_target_device"
 webtalk_add_data -client project -key product_version -value "XSIM v2017.4 (64-bit)" -context "software_version_and_target_device"
 webtalk_add_data -client project -key build_version -value "2086221" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_platform -value "WIN64" -context "software_version_and_target_device"
@@ -12,21 +12,21 @@ webtalk_add_data -client project -key target_family -value "not_applicable" -con
 webtalk_add_data -client project -key target_device -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_package -value "not_applicable" -context "software_version_and_target_device"
 webtalk_add_data -client project -key target_speed -value "not_applicable" -context "software_version_and_target_device"
-webtalk_add_data -client project -key random_id -value "e70f11d4-0b00-4dda-beb5-57bbeb557ed1" -context "software_version_and_target_device"
+webtalk_add_data -client project -key random_id -value "092c77f5-0930-49f7-8d82-22603faa3b70" -context "software_version_and_target_device"
 webtalk_add_data -client project -key project_id -value "30c6137ff39d4f9885df1c3707ab5286" -context "software_version_and_target_device"
-webtalk_add_data -client project -key project_iteration -value "6" -context "software_version_and_target_device"
+webtalk_add_data -client project -key project_iteration -value "8" -context "software_version_and_target_device"
 webtalk_add_data -client project -key os_name -value "Microsoft Windows 8 or later , 64-bit" -context "user_environment"
 webtalk_add_data -client project -key os_release -value "major release  (build 9200)" -context "user_environment"
-webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i3-4030U CPU @ 1.90GHz" -context "user_environment"
-webtalk_add_data -client project -key cpu_speed -value "1896 MHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_name -value "Intel(R) Core(TM) i3-4150 CPU @ 3.50GHz" -context "user_environment"
+webtalk_add_data -client project -key cpu_speed -value "3492 MHz" -context "user_environment"
 webtalk_add_data -client project -key total_processors -value "1" -context "user_environment"
-webtalk_add_data -client project -key system_ram -value "2.000 GB" -context "user_environment"
+webtalk_add_data -client project -key system_ram -value "8.000 GB" -context "user_environment"
 webtalk_register_client -client xsim
 webtalk_add_data -client xsim -key Command -value "xsim" -context "xsim\\command_line_options"
 webtalk_add_data -client xsim -key trace_waveform -value "true" -context "xsim\\usage"
 webtalk_add_data -client xsim -key runtime -value "1 us" -context "xsim\\usage"
 webtalk_add_data -client xsim -key iteration -value "0" -context "xsim\\usage"
 webtalk_add_data -client xsim -key Simulation_Time -value "0.05_sec" -context "xsim\\usage"
-webtalk_add_data -client xsim -key Simulation_Memory -value "5976_KB" -context "xsim\\usage"
-webtalk_transmit -clientid 3293947585 -regid "" -xml C:/Users/Richard/Documents/Schoolwork/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Richard/Documents/Schoolwork/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Richard/Documents/Schoolwork/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
+webtalk_add_data -client xsim -key Simulation_Memory -value "6052_KB" -context "xsim\\usage"
+webtalk_transmit -clientid 152197882 -regid "" -xml C:/Users/Richard/Documents/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/usage_statistics_ext_xsim.xml -html C:/Users/Richard/Documents/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/usage_statistics_ext_xsim.html -wdm C:/Users/Richard/Documents/UCI/Freshman/2018 Winter/EECS 31L/Lab 1 - LFDetector/Lab 1 - LFDetector.sim/sim_1/behav/xsim/xsim.dir/lab1s_tb_vhd_behav/webtalk/usage_statistics_ext_xsim.wdm -intro "<H3>XSIM Usage Report</H3><BR>"
 webtalk_terminate
